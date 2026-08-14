c4ab38727aa8b0723433d2601702b8b94f43c726
Prayas Priyadarshi <prayaspriyadarshi18@gmail.com>
2026-08-14 22:16:00 +0530

    feat(auth): add logout route and clear auth cookie

--- BACKEND/src/controller/auth.controller.js
+++ BACKEND/src/controller/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { findUserByEmail, createUser } from "../dao/user.dao.js";
+import { findUserByEmail, createUser, findUserById } from "../dao/user.dao.js";
 import { CookiesOptions, RefreshTokenCookieOptions } from "../config/config.js";
 
 
